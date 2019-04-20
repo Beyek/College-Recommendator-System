b62defe89f08aff37d18df79d6b4f4f8769b3f03
Vivek Kar Shakya <iceburn.firemelt@gmail.com>
2019-04-20 06:27:47 +0545

    added correlation matrix

--- CollegeRS/collegemodule/views.py
+++ CollegeRS/collegemodule/views.py
@@ -49,94 +49,53 @@ def home(request):
 def college_detail(request):
     try:
         user = request.user
-        # profile = Profile.objects.get(user=user)
-        # preferences = profile.preferences.all()
-        # profile_preference_df = read_frame(preferences)
-        # print(profile_preference_df)
-        # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        # # profile_df = read_frame(profile)
-        # # print(profile_df)
-        # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        # preference = PreferenceCategory.objects.all()
-        # preferences_df = read_frame(preference)
-        # print(preferences_df)
-        # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        # colleges = CollegeAffiliationsPreference.objects.all()
-        # print(CollegeAffiliationsPreference.objects.all())
-        # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        # college_df = read_frame(colleges)
-        # print(college_df)
-        # profile_preference_college_df = profile_preference_df.merge(college_df, left_on="id", right_on="preferenceId")
-        # print(profile_preference_college_df)
-
         profile = Profile.objects.get(user=user)
         preferences = profile.preferences.all()
         profile_preference_df = read_frame(preferences)
         print(profile_preference_df)
-        # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        # # profile_df = read_frame(profile)
-        # # print(profile_df)
-        # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        # preference = PreferenceCategory.objects.all()
-        # preferences_df = read_frame(preference)
-        # print(preferences_df)
-        # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        colleges = CollegeAffiliationsPreference.objects.all()
 
-        # print(CollegeAffiliationsPreference.objects.all())
-        preference_cate = PreferenceCategory.objects.all()
-        df_pre = read_frame(preference_cate)
-        # print(df_pre)
-        # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        colleges = CollegeAffiliationsPreference.objects.all()
         college_df = read_frame(colleges)
         college_df['preferenceId'] = college_df['preferenceId'].apply(lambda x: x.split(".")[0])
         college_df['preferenceId'] = college_df['preferenceId'].astype('int64')
-        # print(college_df)
-        profile_preference_college_df = profile_preference_df.merge(college_df, left_on="id", right_on="preferenceId")
-        # print(profile_preference_college_df)
-        profile_preference_college_df.drop(["id_x", "id_y"], inplace=True, axis=1)
-        # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+        user_pref_similar_colz = profile_preference_df.merge(college_df, left_on="id", right_on="preferenceId")
+        user_pref_similar_colz.drop(["id_x", "id_y"], inplace=True, axis=1)
         print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        # print(profile_preference_college_df.pivot_table(columns='collegeId', index=""))
-        profile_preference_college_df['college_program'] = profile_preference_college_df['collegeId'] + ", "+ \
-                                                           profile_preference_college_df['affiliationId']
+        user_pref_similar_colz['college_program'] = user_pref_similar_colz['collegeId'] + ", "+ \
+                                                    user_pref_similar_colz['affiliationId']
 
-        profile_preference_college_df['preferences'] = profile_preference_college_df['categoryName'] + " - " + \
-                                                       profile_preference_college_df['subCategory'] + " - " + \
-                                                       profile_preference_college_df['value']
+        user_pref_similar_colz['preferences'] = user_pref_similar_colz['categoryName'] + " - " + \
+                                                user_pref_similar_colz['subCategory'] + " - " + \
+                                                user_pref_similar_colz['value']
 
-        profile_preference_college_df = profile_preference_college_df[['preferences', 'college_program']]
-        # print( profile_preference_college_df)
-        # print(profile_preference_college_df.pivot_table(index=['college_program'], values=['preferences'], aggfunc='count'))
-        # print(profile_preference_college_df.groupby('college_program').size().unstack(fill_value=0))
-        final_df = profile_preference_college_df.groupby(['preferences', 'college_program']).size().unstack(fill_value=0.0)
-        print(">>>>>>>>>>>>>>>>>>>>>>>>>")
-        final_df['Best College'] = [1.0,0.0,0.4,0.0,0.9]
-        # recomemded_df = pd.DataFrame(columns=["Best College"],data=np.random.randint(3,5), index=final_df.index)
-        # recomemded_df = pd.DataFrame(columns=["Best College"], data=[1.0,0.3,0.4,0.7,0.9], index=final_df.index)
-        recomemded_df = final_df['Best College']
-        print(recomemded_df)
+        user_pref_similar_colz = user_pref_similar_colz[['preferences', 'college_program']]
+        # print( user_pref_similar_colz)
+        # print(user_pref_similar_colz.pivot_table(index=['college_program'], values=['preferences'], aggfunc='count'))
+        # print(user_pref_similar_colz.groupby('college_program').size().unstack(fill_value=0))
+        final_df = user_pref_similar_colz.groupby(['preferences', 'college_program']).size().unstack(fill_value=0.0000000000000)
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        final_df['Best_College'] = [1.0, 0.9, 0.5, 0.5, 0.7]
+        # final_df['Best_College'] = pd.Series(data=np.random.uniform(0.5, 1.0), index=final_df.index)
+        recommended_df = final_df['Best_College']
+        # recommended_df = pd.DataFrame(data=float(1.0), index=final_df.index)
 
-        print(recomemded_df)
-        print(">>>>>>>>>>>>>>>>>>>>>>>>>")
-        print(">>>>>>>>>>>>>>>>>>>>>>>>>")
+        # recommended_df = final_df['Best_College']
+        print(recommended_df)
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
         print(final_df)
 
-        # for col in final_df.columns:
-        #
-
-        print(recomemded_df)
-        movies_like_forest_gump = final_df.corrwith(recomemded_df)
-        corr_forrest_gump = pd.DataFrame(movies_like_forest_gump, columns=['Correlation'])
-        # corr_forrest_gump.dropna(inplace=True)
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        colz_courses_likedatata = final_df.corrwith(recommended_df)
+        corr_forrest_gump = pd.DataFrame(colz_courses_likedatata, columns=['Correlation'])
         print(corr_forrest_gump.head())
-        corr_forrest_gump.fillna(0, inplace=True)
+        corr_forrest_gump.fillna(0.0, inplace=True)
         corr_forrest_gump_sorted = corr_forrest_gump.sort_values("Correlation", ascending=False)
-        corr_forrest_gump_sorted.drop("Best College", axis=0, inplace=True)
+        corr_forrest_gump_sorted.drop("Best_College", axis=0, inplace=True)
         colleges = list(corr_forrest_gump_sorted.head(5).index)
-        # print(movies_like_forest_gump.head())
+        # print(colz_courses_likedatata.head())
         # return render(request, "",context={"Colleges": colleges})
-        # print(profile_preference_college_df)
+        # print(user_pref_similar_colz)
 
         # profilePreferences = Profile.preferences
         # data = pd.merge(preferences_df, college_df, on='id')
