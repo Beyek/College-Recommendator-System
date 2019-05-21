c6628ebd5c85a0436d43f1e63de18c44bf62617c
Vivek Kar Shakya <iceburn.firemelt@gmail.com>
2019-05-21 23:56:53 +0545

    added optimization. create a seperate module

--- CollegeRS/collegemodule/cosinesimilarity.py
+++ CollegeRS/collegemodule/cosinesimilarity.py
@@ -1,13 +1,59 @@
+import pandas as pd
 from django import db
-from.models import PreferenceCategory, User, CollegeEntityMaster, CollegeAffiliationsPreference
+from django_pandas.io import read_frame
+
+from .models import PreferenceCategory, User, CollegeEntityMaster, CollegeAffiliationsPreference, Profile
 
 
-def findsimilarpreferencefunc(userId):
-    print("Logging message", flush=True)
-    profile = User.objects.get(userId).profile
-    preference = profile.preferences.all()
-    preference_df = preference.to_dataframe()
-    return preference_df.head()
+def findsimilarpreferencefunc(request):
+    user = request.user
+    profile = Profile.objects.get(user=user)
+    preferences = profile.preferences.all()
+    profile_preference_df = read_frame(preferences)
+    print(profile_preference_df)
+
+    colleges = CollegeAffiliationsPreference.objects.all()
+    college_df = read_frame(colleges)
+    college_df['preferenceId'] = college_df['preferenceId'].apply(lambda x : x.split(".")[0])
+    college_df['preferenceId'] = college_df['preferenceId'].astype('int64')
+
+    user_pref_similar_colz = profile_preference_df.merge(college_df, left_on="id", right_on="preferenceId")
+    user_pref_similar_colz.drop(["id_x", "id_y"], inplace=True, axis=1)
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    user_pref_similar_colz['college_program'] = user_pref_similar_colz['collegeId'] + ", " + \
+                                                user_pref_similar_colz['affiliationId']
+
+    user_pref_similar_colz['preferences'] = user_pref_similar_colz['categoryName'] + " - " + \
+                                            user_pref_similar_colz['subCategory'] + " - " + \
+                                            user_pref_similar_colz['value']
+
+    user_pref_similar_colz = user_pref_similar_colz[['preferences', 'college_program']]
+    # print( user_pref_similar_colz)
+    # print(user_pref_similar_colz.pivot_table(index=['college_program'], values=['preferences'], aggfunc='count'))
+    # print(user_pref_similar_colz.groupby('college_program').size().unstack(fill_value=0))
+    final_df = user_pref_similar_colz.groupby(['preferences', 'college_program']).size().unstack(
+        fill_value=0.0)
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    # final_df['Best_College'] = [1.0, 0.9, 0.5, 0.5, 0.7]
+    # final_df['Best_College'] = pd.Series(data=pd.np.random.uniform(0.589898, 0.008879), index=final_df.index)
+    final_df['Best_College'] = pd.Series(data=pd.np.random.uniform(0.789898, 0.887989), index=final_df.index)
+    recommended_df = final_df['Best College, Master of Computer Application(MCA) , Purbanchal University']
+    # recommended_df = pd.DataFrame(data=float(1.0), index=final_df.index)
+
+    # recommended_df = final_df['Best_College']
+    print(recommended_df)
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    print(final_df)
+
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    colz_courses_likedatata = final_df.corrwith(recommended_df)
+    final_user_recommendation = pd.DataFrame(colz_courses_likedatata, columns=['Correlation'])
+    print(final_user_recommendation.head())
+    final_user_recommendation.fillna(0.0, inplace=True)
+    final_user_recommendation_sorted = final_user_recommendation.sort_values("Correlation", ascending=False)
+    final_user_recommendation_sorted.drop("Best_College", axis=0, inplace=True)
+    final_user_recommendation_sorted.drop("Best College, Master of Computer Application(MCA) , Purbanchal University", axis=0, inplace=True)
+    return list(final_user_recommendation_sorted.head(5).index)
 
 
 
--- CollegeRS/collegemodule/views.py
+++ CollegeRS/collegemodule/views.py
@@ -6,6 +6,8 @@ from django.http import Http404
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from django_pandas.io import read_frame
+
+from collegemodule.cosinesimilarity import findsimilarpreferencefunc
 from collegemodule.forms import SignUpForm
 from .models import CollegeEntityMaster, PreferenceCategory, CollegeAffiliationsPreference, Profile
 import numpy as np
@@ -28,7 +30,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=user.username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('college_detail')
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
@@ -48,51 +50,9 @@ def home(request):
 
 def college_detail(request):
     try:
-        user = request.user
-        profile = Profile.objects.get(user=user)
-        preferences = profile.preferences.all()
-        profile_preference_df = read_frame(preferences)
-        print(profile_preference_df)
 
-        colleges = CollegeAffiliationsPreference.objects.all()
-        college_df = read_frame(colleges)
-        college_df['preferenceId'] = college_df['preferenceId'].apply(lambda x: x.split(".")[0])
-        college_df['preferenceId'] = college_df['preferenceId'].astype('int64')
-
-        user_pref_similar_colz = profile_preference_df.merge(college_df, left_on="id", right_on="preferenceId")
-        user_pref_similar_colz.drop(["id_x", "id_y"], inplace=True, axis=1)
-        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        user_pref_similar_colz['college_program'] = user_pref_similar_colz['collegeId'] + ", "+ \
-                                                    user_pref_similar_colz['affiliationId']
-
-        user_pref_similar_colz['preferences'] = user_pref_similar_colz['categoryName'] + " - " + \
-                                                user_pref_similar_colz['subCategory'] + " - " + \
-                                                user_pref_similar_colz['value']
-
-        user_pref_similar_colz = user_pref_similar_colz[['preferences', 'college_program']]
-        # print( user_pref_similar_colz)
-        # print(user_pref_similar_colz.pivot_table(index=['college_program'], values=['preferences'], aggfunc='count'))
-        # print(user_pref_similar_colz.groupby('college_program').size().unstack(fill_value=0))
-        final_df = user_pref_similar_colz.groupby(['preferences', 'college_program']).size().unstack(fill_value=0.0000000000000)
-        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        final_df['Best_College'] = [1.0, 0.9, 0.5, 0.5, 0.7]
-        # final_df['Best_College'] = pd.Series(data=np.random.uniform(0.5, 1.0), index=final_df.index)
-        recommended_df = final_df['Best_College']
-        # recommended_df = pd.DataFrame(data=float(1.0), index=final_df.index)
-
-        # recommended_df = final_df['Best_College']
-        print(recommended_df)
-        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        print(final_df)
-
-        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        colz_courses_likedatata = final_df.corrwith(recommended_df)
-        corr_forrest_gump = pd.DataFrame(colz_courses_likedatata, columns=['Correlation'])
-        print(corr_forrest_gump.head())
-        corr_forrest_gump.fillna(0.0, inplace=True)
-        corr_forrest_gump_sorted = corr_forrest_gump.sort_values("Correlation", ascending=False)
-        corr_forrest_gump_sorted.drop("Best_College", axis=0, inplace=True)
-        colleges = list(corr_forrest_gump_sorted.head(5).index)
+        colleges_list = findsimilarpreferencefunc(request)
+        size = len(colleges_list)
         # print(colz_courses_likedatata.head())
         # return render(request, "",context={"Colleges": colleges})
         # print(user_pref_similar_colz)
@@ -109,4 +69,4 @@ def college_detail(request):
 
     except CollegeEntityMaster.DoesNotExist:
         raise Http404('College not found')
-    return render(request, 'college_details.html', context={"colleges": colleges})
+    return render(request, 'college_details.html', context={"colleges": colleges_list, "size": size})
